f91642a9e19dbade4acbcdfd4ddc05427b4824bf
Dionisio Chavez <dionisiochavez@Dionisios-MacBook-Pro.local>
2021-07-22 16:22:26 -0400

    seccond commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import useRef from './tutorial/5-useRef/setup/1-useRef-basics'
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <h2>Advanced Tutorial v2</h2>
       <useRef />
     </div>
   )
