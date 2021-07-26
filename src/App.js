5acc41a73ab92cf8660589574d526f4cc41d2c67
Dionisio Chavez <dionisiochavez@Dionisios-MacBook-Pro.local>
2021-07-26 09:29:54 -0400

    ract herlmet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import useRef from './tutorial/5-useRef/setup/1-useRef-basics'
+import { Helmet } from 'react-helmet';
+
+import Setup from './tutorial/1-useState/setup/1-error-example'
+import Final from './tutorial/1-useState/final/1-error-example'
+
 function App() {
   return (
     <div className='container'>
+      <Helmet>
+        <title>App Title</title>
+        <meta name="description" content="App Description" />
+        <meta name="theme-color" content="#008f68" />
+      </Helmet>
       <h2>Advanced Tutorial v2</h2>
-      <useRef />
+      <Setup />
+      <Final />
+
     </div>
   )
 }
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
-};
+
+  let title = 'Radom title';
+
+  const clickHandle = () => {
+    title = 'Something else';
+  }
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <button onClick={clickHandle()}>Change</button>
+    </>
+  )
+  
+}
 
 export default ErrorExample;
